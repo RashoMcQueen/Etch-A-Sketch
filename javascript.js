d73677c1a4ca048960c1be61f32f770471b5d5cd
Gibran <themainstayz@gmail.com>
2022-12-19 12:50:41 -0600

    Delete everything, starting with a better approach :(

--- javascript.js
+++ javascript.js
@@ -1,6 +1,4 @@
-let container = document.createElement("div");
-container.classList.add("container");
-document.body.appendChild(container);
+let size = 16;
 
 for (let index = 0; index < 256; index++) {
   let grid = document.createElement("div");
@@ -14,9 +12,16 @@ const gridDivs = document.querySelectorAll(".grid-div");
 
 gridDivs.forEach((gridDiv) => {
   gridDiv.addEventListener("mouseover", changeDivColor);
-  //gridDiv.addEventListener("mouseover", resetDivColor);
 });
 
 function changeDivColor() {
   this.style.backgroundColor = "black";
+}
+
+const gridBtn = document.querySelector("#btn-grid-div button");
+
+gridBtn.addEventListener("click", getPrompGrid);
+
+function getPrompGrid() {
+  prompt("Number of squares per side for the new grid:");
 }
