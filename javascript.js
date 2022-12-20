439d35447629f2783d7a8898ac416ecd53bd1154
Gibran <themainstayz@gmail.com>
2022-12-20 17:16:01 -0600

    Finished project

--- javascript.js
+++ javascript.js
@@ -3,6 +3,7 @@ function populateGrid(size) {
   let grid = document.querySelectorAll(".grid-div");
 
   let amount = size * size;
+  let toggleFlag = true;
   container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
   container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
   grid.forEach((divs) => {
@@ -14,6 +15,7 @@ function populateGrid(size) {
     gridDiv.classList.add("grid-div");
     container.insertAdjacentElement("beforeend", gridDiv);
     gridDiv.addEventListener("mouseover", changeDivColor);
+    gridDiv.addEventListener("click", toggleDrawing(toggleFlag));
   }
 }
 
@@ -23,8 +25,7 @@ function changeDivColor() {
   this.style.backgroundColor = "black";
 }
 
-const gridBtn = document.querySelector("#btn-grid-div button");
-
+const gridBtn = document.querySelector("#new-grid-btn");
 gridBtn.addEventListener("click", getPrompGrid);
 
 function getPrompGrid() {
@@ -37,5 +38,20 @@ function promptValidation(promptAns) {
     alert("Try with a value greater than 1 and less than 101");
   } else {
     populateGrid(promptAns);
+  }
+}
+
+const resetBtn = document.querySelector("#reset-grid-btn");
+resetBtn.addEventListener("click", resetGrid);
+
+function resetGrid() {
+  let grid = document.querySelectorAll(".grid-div");
+  grid.forEach((div) => {
+    div.style.backgroundColor = "white";
+  });
+}
+
+function toggleDrawing(toggleFlag) {
+  if (toggleFlag) {
   }
 }
