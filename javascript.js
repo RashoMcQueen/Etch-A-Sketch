e6a658c116ca1cff00da35e932224f210b79406e
Gibran <themainstayz@gmail.com>
2022-12-19 18:41:38 -0600

    Finished functionality of etch a sketch and creation of new grid

--- javascript.js
+++ javascript.js
@@ -1,18 +1,23 @@
-let size = 16;
+function populateGrid(size) {
+  let container = document.querySelector(".container");
+  let grid = document.querySelectorAll(".grid-div");
 
-for (let index = 0; index < 256; index++) {
-  let grid = document.createElement("div");
-  grid.classList.add("grid-div");
-  document.querySelector(".container").appendChild(grid);
+  let amount = size * size;
+  container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+  grid.forEach((divs) => {
+    divs.remove();
+  });
+
+  for (let index = 0; index < amount; index++) {
+    let gridDiv = document.createElement("div");
+    gridDiv.classList.add("grid-div");
+    container.insertAdjacentElement("beforeend", gridDiv);
+    gridDiv.addEventListener("mouseover", changeDivColor);
+  }
 }
 
-//E V E N T S
-
-const gridDivs = document.querySelectorAll(".grid-div");
-
-gridDivs.forEach((gridDiv) => {
-  gridDiv.addEventListener("mouseover", changeDivColor);
-});
+populateGrid(16);
 
 function changeDivColor() {
   this.style.backgroundColor = "black";
@@ -23,5 +28,14 @@ const gridBtn = document.querySelector("#btn-grid-div button");
 gridBtn.addEventListener("click", getPrompGrid);
 
 function getPrompGrid() {
-  prompt("Number of squares per side for the new grid:");
+  let promptAns = prompt("Number of squares per side for the new grid:");
+  promptValidation(promptAns);
+}
+
+function promptValidation(promptAns) {
+  if (promptAns < 2 || promptAns > 100) {
+    alert("Try with a value greater than 1 and less than 101");
+  } else {
+    populateGrid(promptAns);
+  }
 }
